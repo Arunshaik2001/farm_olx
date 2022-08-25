4a5bd534a1f5e862881066ceae708126809acfc4
dhairya <20032501@nurture.farm>
2022-08-25 21:28:43 +0530

    Updated group registration widget

--- lib/farmer_accordion.dart
+++ lib/farmer_accordion.dart
@@ -1,91 +1,219 @@
-
-
-import 'package:accordion/accordion.dart';
-import 'package:accordion/accordion_section.dart';
-import 'package:accordion/controllers.dart';
 import 'package:flutter/material.dart';
 
-/// Main example page
-class AccordionPage extends StatefulWidget //__
-    {
-  const AccordionPage({Key? key}) : super(key: key);
-
+class MyForm extends StatefulWidget {
   @override
-  State<AccordionPage> createState() => _AccordionPageState();
+  _MyFormState createState() => _MyFormState();
 }
 
-class _AccordionPageState extends State<AccordionPage> {
-  final _headerStyle = const TextStyle(
-      color: Color(0xffffffff), fontSize: 15, fontWeight: FontWeight.bold);
-
-  final _contentStyleHeader = const TextStyle(
-      color: Color(0xff999999), fontSize: 14, fontWeight: FontWeight.w700);
-
-  final _contentStyle = const TextStyle(
-      color: Color(0xff999999), fontSize: 14, fontWeight: FontWeight.normal);
-
-  final _loremIpsum =
-  '''Lorem ipsum is typically a corrupted version of 'De finibus bonorum et malorum', a 1st century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin.''';
-
-  final List<AccordionSection> farmerAccordionList = [];
+class _MyFormState extends State<MyForm> {
+  final _formKey = GlobalKey<FormState>();
+  late TextEditingController _nameController;
+  late TextEditingController _cropController;
+  static List<String> farmersList = [""];
+  static List<String> acresList = [""];
+  static List<String> idCardList = [""];
 
   @override
   void initState() {
-    farmerAccordionList.add(AccordionSection(
-      isOpen: true,
-      leftIcon: const Icon(Icons.insights_rounded, color: Colors.white),
-      headerBackgroundColor: Colors.black,
-      headerBackgroundColorOpened: Colors.red,
-      header: Text('Farmer', style: _headerStyle),
-      content: farmerForm(),
-      contentHorizontalPadding: 20,
-      contentBorderWidth: 1,
-      // onOpenSection: () => print('onOpenSection ...'),
-      // onCloseSection: () => print('onCloseSection ...'),
-    ));
     super.initState();
+    _nameController = TextEditingController();
+    _cropController = TextEditingController();
   }
 
   @override
-  build(context) => Scaffold(
-    backgroundColor: Colors.blueGrey[100],
-    appBar: AppBar(
-      title: const Text('Accordion'),
-    ),
-    body: Column(
-      children: [
-        Accordion(
-          maxOpenSections: 1,
-          headerBackgroundColorOpened: Colors.black54,
-          scaleWhenAnimating: true,
-          openAndCloseAnimation: true,
-          headerPadding:
-          const EdgeInsets.symmetric(vertical: 7, horizontal: 15),
-          sectionOpeningHapticFeedback: SectionHapticFeedback.heavy,
-          sectionClosingHapticFeedback: SectionHapticFeedback.light,
-          children: farmerAccordionList,
-        ),
-        Container(
-            decoration: BoxDecoration(
-                color: Colors.deepPurple,
-                borderRadius: BorderRadius.circular(8)),
-            child: MaterialButton(
-              onPressed: () {
-                farmerAccordionList.add(farmerAccordionList[0]);
-                setState(() {});
-              },
-              child: const Text(
-                "Add Farmer",
-                style: TextStyle(color: Colors.white),
-              ),
-            ))
-      ],
-    ),
-  );
+  void dispose() {
+    _nameController.dispose();
+    _cropController.dispose();
+    super.dispose();
+  }
 
-  Widget farmerForm() {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(title: const Text('Dynamic TextFormFields'),),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // name textfield
+                Padding(
+                  padding: const EdgeInsets.only(right: 32.0),
+                  child: TextFormField(
+                    controller: _nameController,
+                    decoration: const InputDecoration(
+                        hintText: 'Enter group name'
+                    ),
+                    validator: (v){
+                      if(v!.trim().isEmpty) return 'Please enter something';
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 32.0),
+                  child: TextFormField(
+                    controller: _cropController,
+                    decoration: const InputDecoration(
+                        hintText: 'Enter crop name'
+                    ),
+                    validator: (v){
+                      if(v!.trim().isEmpty) return 'Please enter something';
+                      return null;
+                    },
+                  ),
+                ),
+                ..._getFriends(),
+                const SizedBox(height: 40,),
+                FlatButton(
+                  onPressed: (){
+                    if(_formKey.currentState!.validate()){
+                      _formKey.currentState!.save();
+                    }
+                  },
+                  child: const Text('Submit'),
+                  color: Colors.green,
+                ),
+
+              ],
+            ),
+          ),
+        ),
+      ),
+
+    );
+  }
+
+  /// get firends text-fields
+  List<Widget> _getFriends(){
+    List<Widget> friendsTextFields = [];
+    for(int i=0; i<farmersList.length; i++){
+      friendsTextFields.add(
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Row(
+              children: [
+                Expanded(child: FriendTextFields(i)),
+                const SizedBox(width: 16,),
+                // we need add button at last friends row
+                _addRemoveButton(i == farmersList.length-1, i),
+              ],
+            ),
+          )
+      );
+    }
+    return friendsTextFields;
+  }
+
+  /// add / remove button
+  Widget _addRemoveButton(bool add, int index){
+    return InkWell(
+      onTap: (){
+        if(add){
+          // add new text-fields at the top of all friends textfields
+          farmersList.insert(index+1, "");
+          acresList.insert(index+1, "");
+          idCardList.insert(index+1, "");
+        }
+        else {
+          farmersList.removeAt(index);
+          acresList.removeAt(index);
+          idCardList.removeAt(index);
+        }
+        setState((){});
+      },
+      child: Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(
+          color: (add) ? Colors.green : Colors.red,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Icon((add) ? Icons.add : Icons.remove, color: Colors.white,),
+      ),
+    );
+  }
+
+
+}
+
+class FriendTextFields extends StatefulWidget {
+  final int index;
+  FriendTextFields(this.index);
+  @override
+  _FriendTextFieldsState createState() => _FriendTextFieldsState();
+}
+
+class _FriendTextFieldsState extends State<FriendTextFields> {
+  late TextEditingController _nameController;
+  late TextEditingController _acresController;
+  late TextEditingController _idCardController;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController();
+    _acresController = TextEditingController();
+    _idCardController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _acresController.dispose();
+    _idCardController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      _nameController.text = _MyFormState.farmersList[widget.index] ?? '';
+      _acresController.text = _MyFormState.acresList[widget.index] ?? '';
+      _idCardController.text = _MyFormState.idCardList[widget.index] ?? '';
+    });
+
     return Column(
-      children: const [TextField(), TextField(), TextField()],
+      children: [
+        TextFormField(
+          controller: _nameController,
+          onChanged: (v) => _MyFormState.farmersList[widget.index] = v,
+          decoration: const InputDecoration(
+              hintText: 'Enter farmer name'
+          ),
+          validator: (v){
+            if(v!.trim().isEmpty) return 'Please enter something';
+            return null;
+          },
+        ),
+        TextFormField(
+          controller: _acresController,
+          onChanged: (v) => _MyFormState.acresList[widget.index] = v,
+          decoration: const InputDecoration(
+              hintText: 'Enter farm acres'
+          ),
+          validator: (v){
+            if(v!.trim().isEmpty) return 'Please enter something';
+            return null;
+          },
+        ),
+        TextFormField(
+          controller: _idCardController,
+          onChanged: (v) => _MyFormState.idCardList[widget.index] = v,
+          decoration: const InputDecoration(
+              hintText: 'Enter PAN number'
+          ),
+          validator: (v){
+            if(v!.trim().isEmpty) return 'Please enter something';
+            return null;
+          },
+        ),
+      ],
     );
   }
 }
